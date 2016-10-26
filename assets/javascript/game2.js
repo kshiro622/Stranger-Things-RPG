bf2627438accbc07f55611580d9a3455a5e917c0
Katherine Shiro <katie.shiro@gmail.com>
2016-10-25 20:04:00 -0500

    Objects character info stored in array of objects function to render dom initial selecting and moving characters in process

---
+++ assets/javascript/game2.js
@@ -0,0 +1,166 @@
+$(document).ready(function(){
+
+// character data
+	var characters = [
+		{
+			name: 'Eleven',
+			image: 
+			health: 120,
+			attack: 8,
+			counterAttack: 15,
+			isCharacter: false,
+			isDefender: false
+		},
+
+		{
+			name: 'Chief Hopper',
+			image:
+			health: 100,
+			attack: 5,
+			counterAttack: 10,
+			isCharacter: false,
+			isDefender: false
+		},
+
+		{
+			name: 'Monster',
+			image:
+			health: 180,
+			attack: 25,
+			counterAttack: 25,
+			isCharacter: false,
+			isDefender: false
+		},
+
+		{
+			name: 'Dr. Brenner',
+			image:
+			health: 150,
+			attack: 16,
+			counterAttack: 20,
+			isCharacter: false,
+			isDefender: false
+		}
+	];
+
+	var characterIsChosen = false;
+	var defenderIsChosen = false;
+
+//initialize game
+
+function initialize(){
+	//inner HTML print to DOM 
+	for (var i = 0; i<characters.length; i++) {
+
+	}
+	//on click functions
+}
+
+// choose character, move others to enemies
+	if (characterIsChosen === false) {
+		$('#eleven').on('click', function() {
+			$(this).animate({top:'+=137px'});
+			$('#chief-hopper').animate({top:'+=285', left:'-=130px', color:'#ee2e24'});
+			$('#monster').animate({top:'+=285', left:'-=130px'});
+			$('#dr-brenner').animate({top:'+=285', left:'-=130px'});
+			$('#chief-hopper').addClass('enemy');
+			$('#monster').addClass('enemy');
+			$('#dr-brenner').addClass('enemy');
+			characterIsChosen = true;
+			eleven.isCharacter = true;
+			console.log(characterIsChosen);
+		});
+
+		$('#chief-hopper').on('click', function() {
+			$(this).animate({top:'+=137px', left:'-=130px'});
+			$('#eleven').animate({top:'+=285'});
+			$('#monster').animate({top:'+=285', left:'-=130px'});
+			$('#dr-brenner').animate({top:'+=285', left:'-=130px'});
+			$('#eleven').addClass('enemy');
+			$('#monster').addClass('enemy');
+			$('#dr-brenner').addClass('enemy');
+			characterIsChosen = true;
+			chiefHopper.isCharacter = true;
+			console.log(characterIsChosen);
+		});
+
+		$('#monster').on('click', function() {
+			$(this).animate({top:'+=137px', left:'-=260px'});
+			$('#eleven').animate({top:'+=285'});
+			$('#chief-hopper').animate({top:'+=285'});
+			$('#dr-brenner').animate({top:'+=285', left:'-=130px'});
+			$('#eleven').addClass('enemy');
+			$('#chief-hopper').addClass('enemy');
+			$('#dr-brenner').addClass('enemy');
+			characterIsChosen = true;
+			monster.isCharacter = true;
+			console.log(characterIsChosen);
+		});
+
+		$('#dr-brenner').on('click', function() {
+			$(this).animate({top:'+=137px', left:'-=390px'});
+			$('#eleven').animate({top:'+=285'});
+			$('#chief-hopper').animate({top:'+=285'});
+			$('#monster').animate({top:'+=285'});
+			$('#eleven').addClass('enemy');
+			$('#chief-hopper').addClass('enemy');
+			$('#monster').addClass('enemy');
+			characterIsChosen = true;
+			drBrenner.isCharacter = true;
+			return characterIsChosen;
+		});
+
+// move character to defender position 
+	} else if ((characterIsChosen === true) && (defenderIsChosen === false)) {
+		$('#eleven').on('click', function() {
+			$(this).animate({top:'+=137px'});
+			$(this).addClass('defender');
+			eleven.isDefender = true;
+			defenderIsChosen = true;
+		});
+
+		//...
+	};
+
+// attack button --> 
+	$('#attack').on('click', function(character, defender) {
+		character.health = character.health - defender.counterAttack;
+		defender.health = defender.health - character.attack;
+		character.attack = character.attack * 2;//something to increase exponentially
+	});
+
+	if (eleven.isCharacter === true){ 
+		if (chiefHopper.isDefender === true) {
+			attack(eleven, chiefHopper);
+		} else if (monster.isDefender === true) {
+			attack(eleven, monster);
+		} else if (drBrenner.isDefender === true) {
+			attack(eleven, drBrenner);
+		}
+	
+		if (chiefHopper.isCharacter === true){
+			//...
+		}
+
+		if (monster.isCharacter === true){
+			//...
+		} 
+
+		if (drBrenner.isCharacter === true){
+			//...
+		}
+	};
+
+// restart button
+	$('#restart').on('click', function() {
+		$('#eleven').animate({top: '5px'});
+		$('#chief-hopper').animate({top:'5px', left:'140px'});
+		$('#monster').animate({top:'5px', left:'270px'});
+		$('#dr-brenner').animate({top:'5px', left:'400px'});
+		$('#eleven').removeClass('enemy', 'defender');
+		$('#chief-hopper').removeClass('enemy', 'defender');
+		$('#monster').removeClass('enemy', 'defender');
+		$('#dr-brenner').removeClass('enemy', 'defender');
+	});
+});
+
